504c0009ae87771c4930a48ba8ba5dacad8d4202
eyadal <31968847+eyadal@users.noreply.github.com>
2022-11-19 00:27:01 +0100

    creat variables for my html elements in the js file.

---
+++ 03-JavaScript -calculator/index.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('input'), // input/output button
+  number = document.querySelectorAll('.numbers div'), // number buttons
+  operator = document.querySelectorAll('.operators div'), // operator buttons
+  result = document.getElementById('result'), // equal button
+  clear = document.getElementById('clear'), // clear button
+  resultDisplayed = false // flag to keep an eye on what output is displayed
